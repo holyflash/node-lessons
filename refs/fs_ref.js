3bde5a01997a25ddfb37a80d608fbdd2841ff446
holyflash <arxipovpavel@gmail.com>
2020-05-06 04:28:29 +0300

    Test Commit

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -21,26 +21,26 @@ const path = require("path");
 
 // console.log(123);
 
-let file_path = path.join(__dirname, "./notes", "mynote.txt");
+let file_path = path.join(__dirname, "./notes", "note.txt");
 
-// fs.writeFile(file_path, "Hello world", err => {
-// 	if (err) throw err
-// 	console.log("Создан файл")
+fs.writeFile(file_path, "Hello world", err => {
+	if (err) throw err
+	console.log("Создан файл")
 
-// 	fs.appendFile(file_path, " From append file", err => {
-// 		if (err) throw err
-// 		console.log("Изменен файл")
+	fs.appendFile(file_path, " From append file", err => {
+		if (err) throw err
+		console.log("Изменен файл")
 
-// 		fs.readFile(file_path, "utf-8", (err, data) => {
-// 			if(err) throw err
-// 			console.log(data);
-// 		})
-// 	})
-// })
-
-
-fs.rename(file_path, path.join(__dirname, "./notes", "./notes.txt"), err => {
-	if(err) return console.log("Ошибка");
-
-	console.log("Переименовано")
+		fs.readFile(file_path, "utf-8", (err, data) => {
+			if(err) throw err
+			console.log(data);
+		})
+	})
 })
+
+
+// fs.rename(file_path, path.join(__dirname, "./notes", "./notes.txt"), err => {
+// 	if(err) return console.log("Ошибка");
+
+// 	console.log("Переименовано")
+// })
